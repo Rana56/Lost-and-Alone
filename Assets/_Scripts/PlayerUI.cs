4da3bb52f554557c713bf2521bca17c82e490795
Chirag <54218819+Rana56@users.noreply.github.com>
2023-03-29 21:34:49 +0100

    1.2.5
    Item Collection
    Environment update

--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -7,12 +7,25 @@ using UnityEngine;
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text timeText;
+    [SerializeField] private TMP_Text gemText;
+    private int totalGems;
+    private int collectedGem = 0;
+
+    void Start(){
+        totalGems = GameObject.FindGameObjectsWithTag("Gem").Length;
+        gemText.text = string.Format("Gems: {0}/{1}", collectedGem, totalGems);
+    }
 
     void Update(){
         float t = Time.timeSinceLevelLoad;
 		int mins = (int)( t / 60 );
 		int rest = (int)(t % 60);
 		timeText.text = string.Format("Playtime - {0:D2}:{1:D2}", mins, rest);
+    }
+
+    public void UpdateGemText(PlayerInventory player){
+        collectedGem = player.NumberOfGems;
+        gemText.text = string.Format("Gems: {0}/{1}", collectedGem, totalGems);
     }
 }
 
